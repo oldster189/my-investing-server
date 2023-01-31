6abab6e7b9519968fb209cae932f604031d30b7b
Itthiphon Wiwatthanasathit <oldster189@gmail.com>
2023-02-01 01:22:40 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalInterceptors(new TransformReponseInterceptor())
 
-  await app.listen(3000)
+  await app.listen(8080)
 }
 bootstrap()
