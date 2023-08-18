e1175ed7f83136f5b6d6773abe53c0aaa944b39c
Itthiphon Wiwatthanasathit <oldster189@gmail.com>
2023-08-18 22:29:03 +0700

    .

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,10 +4,10 @@ import { Cron, CronExpression } from '@nestjs/schedule'
 
 @Injectable()
 export class TasksService {
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_3_HOURS)
   async checkCurrentPriceStockFromSuperInvestor() {
     try {
-      sendLineNotify('test', 'NTGFpYYz6MZ1mHUFRw27hAmxjW0BkkTBL33J1ceqLn9')
+      sendLineNotify('test', 'ZYbjAHGznzVLn9rbrbnanxVuVRHNUclOAKntahtdR4f')
     } catch (error) {}
   }
 }
