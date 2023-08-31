7f0f056c9f20eb433ecb93c7873739dc45482db6
Itthiphon Wiwatthanasathit <oldster189@gmail.com>
2023-08-31 21:55:51 +0700

    .

--- src/watch-lists/watch-lists.module.ts
+++ src/watch-lists/watch-lists.module.ts
@@ -4,6 +4,7 @@ import { Stocks, StocksSchema } from 'src/stocks/schemas/stocks.schema'
 import { WatchList, WatchListSchema } from './schemas/watch-list.schema'
 import { WatchListsController } from './watch-lists.controller'
 import { WatchListsService } from './watch-lists.service'
+import { StocksModule } from 'src/stocks/stocks.module'
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { WatchListsService } from './watch-lists.service'
       { name: WatchList.name, schema: WatchListSchema },
       { name: Stocks.name, schema: StocksSchema },
     ]),
+    StocksModule,
   ],
   controllers: [WatchListsController],
   providers: [WatchListsService],
