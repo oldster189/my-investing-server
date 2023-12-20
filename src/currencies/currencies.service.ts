7b8e4ff4140f3294b4e9875396442632a5c04dd3
Itthiphon Wiwatthanasathit <oldster189@gmail.com>
2023-12-20 22:16:54 +0700

    .

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -15,7 +15,8 @@ export class CurrenciesService {
 
   async getExchangeRate(): Promise<any> {
     try {
-      const url = `https://bank.kkpfg.com/Utility/GetExchangeRate?date=${dayjs().toISOString()}&lang=th&firstTime=true`
+
+      const url = `https://bank.kkpfg.com/Utility/GetExchangeRate?date=${dayjs().toISOString()}&lang=th&firstTime=false`
       const { data: response } = await axios.get<ExchangeRate>(url)
       const usdDetail = response.data[response.data.length - 1].detail.find((detail) => detail.currencyCode === 'USD')
       if (!usdDetail) throw new BadRequestException('Exchange rate not found')
